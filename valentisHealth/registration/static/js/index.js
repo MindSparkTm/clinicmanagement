e6b8abeb3e2554ca2431187c51dcbc1c42550527
stephen <stephen@redpulse.co.ke>
2018-01-26 17:41:26 +0300

    Move search and Authentication form to seperate views

--- valentisHealth/registration/static/js/index.js
+++ valentisHealth/registration/static/js/index.js
@@ -1,3 +1,11 @@
-document.getElementById("uploadBtn").onchange = function () {
-    document.getElementById("uploadFile").value = this.files[0].name;
-};
+// document.getElementById("uploadBtn").onchange = function () {
+//     document.getElementById("uploadFile").value = this.files[0].name;
+// };
+
+$(document).ready(function($) {
+    console.log("clicked")
+    $(document).on( 'click', ".clickable-row", function() {
+        console.log("clicked")
+        window.location = $(this).data("href");
+    });
+});
