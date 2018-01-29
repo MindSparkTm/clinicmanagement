b21db967e1c5f4931379ed5d5f3695ab39cfacfc
Jackson Ndirangu <jackson@redpulse.co.ke>
2018-01-29 11:56:30 +0300

    Admission form pdf
    show covers limits and balance
    display sanlam logo on the preauthorization form

--- valentisHealth/account/views.py
+++ valentisHealth/account/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import View
-from django.contrib.auth import authenticate, login
-from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, reverse
 from django.contrib import messages
 from urllib.parse import urlencode
+
+from io import StringIO
+from django.template.loader import get_template
+from django.http import HttpResponse
+from xhtml2pdf import pisa
+from django.utils.html import escape
 # from django.conf import settings
 
 from .forms import LoginForm
@@ -46,3 +52,36 @@ class LoginPage(View):
         else:
             form = LoginForm()
         return render(request, "login.html", {'form': form})
+
+
+class Logout(View):
+    def get(self, request):
+        logout(request)
+        return redirect(reverse('login'))
+
+def render_to_pdf(template_src, context_dict, action='view'):
+    template = get_template(template_src)
+    context = context_dict
+    html = template.render(context)
+
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'inline; filename="lpo.pdf"'
+
+    pisaStatus = pisa.CreatePDF(
+        html, dest=response)
+
+    if pisaStatus.err:
+        return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    else:
+        return response
+
+
+def print_pdf(request):
+    d = {
+        'pagesize': 'A4',
+        'supplier': '',
+        'item_details': '',
+        'lpo': '',
+        'site_name': 'name'
+    }
+    return render_to_pdf('pdf.html', d)
