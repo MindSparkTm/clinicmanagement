470172d5b1b8e097a59b674e38912dddd3a3f41a
Ted <ted@redpulse.co.ke>
2018-02-22 11:32:37 +0300

    Added the Superadmin template and related css files

--- valentisHealth/account/urls.py
+++ valentisHealth/account/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url('^log-in/$', LoginPage.as_view(), name='login'),
     url(r'^pdf', print_pdf, name='print'),
     url(r'^logout', Logout.as_view(), name='logout'),
+    url(r'^adduser', AddUser.as_view(), name='adduser'),
 
 ]
