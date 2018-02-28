0acac958d5230d470487b5ded67c74d1892533ba
stephen <stephen@redpulse.co.ke>
2018-02-28 16:42:03 +0300

    Include Allergies

--- valentisHealth/registration/dbmodels.py
+++ valentisHealth/registration/dbmodels.py
@@ -10,25 +10,34 @@ with open('county.csv') as csvfile:
     for row in reader:
         print(row['County'])
         cnty = row['County']
-        p = County.objects.create(county_name=cnty)
+        p = County.objects.create(County=cnty)
         p.save()
 
-
- path = "/Users/redpulse/Documents/ValentisHealth/valentishealth/valentisHealth"
-
-
+path = "/Users/redpulse/Documents/ValentisHealth/valentishealth/valentisHealth"
 
 import os, csv
+
 # from registration.models import InsuranceCompanies
 
 path = "/opt/valentisHealth/"
 
 os.chdir(path)
 
-with open('medicalinsurance.csv') as csvfile:
+with open('Allergies.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print(row['Name'])
-        p = InsuranceCompanies.objects.create(Name=row['Name'])
+        print(row['allergy_name'])
+        p = Allergies.objects.create(allergy_name=row['allergy_name'])
+        p.save()
+
+
+path = "/opt/valentisHealth/"
+os.chdir(path)
+
+with open('Allergies.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row)
+        p = County.objects.create(allergy_name=row['allergy_name'])
         p.save()
 
