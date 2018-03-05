89ab71eafe78fdef11a9e477549f15c0d67df8de
stephen <stephen@redpulse.co.ke>
2018-03-05 13:36:33 +0300

    Fix callcenter permissions

--- valentisHealth/valentisHealth/authenticator.py
+++ valentisHealth/valentisHealth/authenticator.py
@@ -1,22 +1,22 @@
 from django.db.models import Q
 
 def is_labs(request):
-    return request.user.groups.filter(Q(name='Labs') | Q(name='Admin') | Q(name='Superadmin')).exists()
+    return request.user.groups.filter(Q(name='Labs') | Q(name='Callcenter') | Q(name='Admin') | Q(name='Superadmin')).exists()
 
 def is_radiology(request):
-    return request.user.groups.filter(Q(name='Radiologist') | Q(name='Admin') | Q(name='Superadmin')).exists()
+    return request.user.groups.filter(Q(name='Radiologist') | Q(name='Callcenter') | Q(name='Admin') | Q(name='Superadmin')).exists()
 
 def is_doctor(request):
-    return request.user.groups.filter(Q(name='Doctor') | Q(name='Admin') | Q(name='Superadmin')).exists()
+    return request.user.groups.filter(Q(name='Doctor') | Q(name='Callcenter') | Q(name='Admin') | Q(name='Superadmin')).exists()
 
 def is_callcenter(request):
     return request.user.groups.filter(Q(name='Callcenter') | Q(name='Admin') | Q(name='Superadmin')).exists()
 
 def is_nurse(request):
-    return request.user.groups.filter(Q(name='Nurse') | Q(name='Admin') | Q(name='Superadmin')).exists()
+    return request.user.groups.filter(Q(name='Nurse') | Q(name='Callcenter') | Q(name='Admin') | Q(name='Superadmin')).exists()
 
 def is_receptionist(request):
-    return request.user.groups.filter(Q(name='Receptionist') | Q(name='Admin') | Q(name='Superadmin')).exists()
+    return request.user.groups.filter(Q(name='Receptionist') | Q(name='Callcenter') | Q(name='Admin') | Q(name='Superadmin')).exists()
 
 def is_admin(request):
     return request.user.groups.filter(Q(name='Admin') | Q(name='Superadmin')).exists()
