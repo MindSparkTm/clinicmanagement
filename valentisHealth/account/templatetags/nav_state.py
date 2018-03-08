25d9a931f893e6e21552863035626e746c499b15
stephen <stephen@redpulse.co.ke>
2018-03-08 13:09:02 +0300

    Fix navigation higlighting

--- valentisHealth/account/templatetags/nav_state.py
+++ valentisHealth/account/templatetags/nav_state.py
@@ -11,11 +11,15 @@ def nav_active(request, url):
     """
     In template: {% nav_active request "url_name_here" %}
     """
+    path_object = request.path.split('/')[:3]
     url_name = resolve(request.path).url_name
-    if url_name == url:
-        return 'active'
-    # elif url_name.url.split('/')[:1] == url.split('/')[:1]:
+    # if url_name == url:
     #     return 'active'
+    if url == 'labs':
+        path_object = path_object[1:]
+
+    if url in request.path.split('/')[:3]:
+        return 'active'
     return ''
 
 
@@ -23,15 +27,15 @@ def nav_active(request, url):
 def has_group(request):
 
     all_links = {
-        "registration": """<a href="/registration/" class=\""""+nav_active(request,"registration_search")+"""\"  ><i class="fa fa-user-plus spav"> </i> Registration   </a>""",
+        "registration": """<a href="/registration/" class=\""""+nav_active(request,"registration")+"""\"  ><i class="fa fa-user-plus spav"> </i> Registration   </a>""",
 
-        "triage": """<a href="/nurse/models/create/"  class=\""""+nav_active(request,"nurse_triage_create")+"""\" > <i class="fa fa-address-book spav"> </i>  Triage  </a>""",
-        "clinic": """<a href="/clinic/patientvisit/create/"  class=\""""+nav_active(request,'doctor_visit')+"""\"  > <i class="fa fa-clipboard spav"> </i> Clinic</a>""",
-        "labs": """<a href="/labs/" class=\""""+nav_active(request,'labs_list')+"""\"> <i class="fa fa-flask spav"> </i>  Labs  </a>""",
-        "radiology": """<a href="/labs/radiology/" class=\""""+nav_active(request,'radiology_list')+""""> <i class="fa fa-bolt spav"> </i> Radiology </a>""",
-        "prescription": """<a href="/medication/search/" class=\""""+nav_active(request,'medication_models_search')+"""\"> <i class="fa fa-book spav"> </i> Prescriptions </a>""",
-        "payments": """<a href="/payments/search_member/" class=\""""+nav_active(request,'radiology_list')+""""> <i class="fa fa-book spav"> </i> Authorization </a>""",
-        "admin": """<a href="/account/admin/adduser" class=\""""+nav_active(request,'adduser')+""""> <i class="fa fa-book spav"> </i> Admin </a>""",
+        "triage": """<a href="/nurse/models/create/"  class=\""""+nav_active(request,"nurse")+"""\" > <i class="fa fa-address-book spav"> </i>  Triage  </a>""",
+        "clinic": """<a href="/clinic/patientvisit/create/"  class=\""""+nav_active(request,'clinic')+"""\"  > <i class="fa fa-clipboard spav"> </i> Clinic</a>""",
+        "labs": """<a href="/labs/" class=\""""+nav_active(request,'labs')+"""\"> <i class="fa fa-flask spav"> </i>  Labs  </a>""",
+        "radiology": """<a href="/labs/radiology/" class=\""""+nav_active(request,'radiology')+""""> <i class="fa fa-bolt spav"> </i> Radiology </a>""",
+        "prescription": """<a href="/medication/search/" class=\""""+nav_active(request,'medication')+"""\"> <i class="fa fa-book spav"> </i> Prescriptions </a>""",
+        "payments": """<a href="/payments/search_member/" class=\""""+nav_active(request,'payments')+""""> <i class="fa fa-book spav"> </i> Authorization </a>""",
+        "admin": """<a href="/account/admin/adduser" class=\""""+nav_active(request,'account')+""""> <i class="fa fa-book spav"> </i> Admin </a>""",
         }
 
     if is_admin(request):
