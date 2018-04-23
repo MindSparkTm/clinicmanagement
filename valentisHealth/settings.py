ff6a8fe221477cf3c4442420b6dca770b5be4864
Surajit <smartsurajit2008@gmail.com>
2018-04-23 17:24:05 +0300

    fixed prescription module

--- valentisHealth/settings.py
+++ valentisHealth/settings.py
@@ -180,16 +180,16 @@ EMAIL_DEBUG = True
 # SESSION_IDLE_TIMEOUT = TIME  #logout
 
 
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-    ),
-}
+#REST_FRAMEWORK = {
+# 'DEFAULT_PERMISSION_CLASSES': (
+#    'rest_framework.permissions.IsAuthenticated',
+# ),
+# 'DEFAULT_AUTHENTICATION_CLASSES': (
+#   'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+#   'rest_framework.authentication.SessionAuthentication',
+#   'rest_framework.authentication.BasicAuthentication',
+#),
+#}
 
 JWT_AUTH = {
     'JWT_ALLOW_REFRESH': True,
